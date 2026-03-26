77dbe719cc2385a9c47e8554ce4ef20b605a32cf
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 22:04:39 +0530

    Create PrintInReverseOrder.java

---
+++ strings/PrintInReverseOrder.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        String firWord = input.nextLine();
+        String secWord = input.nextLine();
+        System.out.println(secWord);
+        System.out.println(firWord);
+    }
+}
