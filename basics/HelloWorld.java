91e0f34d6cfffd09c97423390b4cf7f6524bae32
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 22:17:01 +0530

    Update HelloWorld.java

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+public class HelloWorld {
     public static void main(String[] args) { 
         // Write your code here
         Scanner sc = new Scanner(System.in);
