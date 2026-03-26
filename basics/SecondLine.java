d5def5d33f9e03f77a0695ea5430a2d237dcc942
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 22:20:41 +0530

    Update SecondLine.java

--- basics/SecondLine.java
+++ basics/SecondLine.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class SecondLine {
     public static void main(String[] args) { 
         Scanner string = new Scanner(System.in);
-        
         String firstString = string.nextLine();
         String secondString = string.nextLine();
         System.out.println(secondString);
