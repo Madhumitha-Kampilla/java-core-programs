294fd439c929525f90fa7fc5e22966f94a038d70
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 22:06:50 +0530

    Create LastCharacterInString.java

---
+++ strings/LastCharacterInString.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        String word = str.nextLine();
+        int len = word.length();
+        System.out.println(word.charAt(len - 1));
+    }
+}
