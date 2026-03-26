4257f1360bacdb93a8d0084af0a3e73fa6873d60
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 21:11:16 +0530

    Create SecondLine.java

---
+++ basics/SecondLine.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner string = new Scanner(System.in);
+        
+        String firstString = string.nextLine();
+        String secondString = string.nextLine();
+        System.out.println(secondString);
+    }
+}
