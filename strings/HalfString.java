9a33c3007573459f72d88fa668361d93b256eaab
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 22:07:28 +0530

    Create HalfString.java

---
+++ strings/HalfString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner str = new Scanner(System.in);
+        String word = str.nextLine();
+        int len = word.length();
+        int half_str = len/2;
+        System.out.println(word.substring(0,half_str));
+    }
+}
