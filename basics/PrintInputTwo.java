7a25586f861214792223a1642b56c5039adb6db0
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-26 22:20:28 +0530

    Update PrintInputTwo.java

--- basics/PrintInputTwo.java
+++ basics/PrintInputTwo.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class PrintInputTwo {
     public static void main(String[] args) { 
-        // Write your code here
         Scanner input = new Scanner(System.in);
         String word = input.nextLine();
         System.out.println("Given input" + ": " + word);
