7e91534fba943fd286425fa1dea41ea420a5aaba
Madhumitha-Kampilla <kampillamadhumitha4@gmail.com>
2026-03-27 23:37:31 +0530

    Create TransposeMatrix.java

---
+++ arrays/TransposeMatrix.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TransposeMatrix{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int m = input.nextInt();
+        int n = input.nextInt();
+        
+        int[][] arr= new int[m][n];
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                arr[i][j] = input.nextInt();
+            }
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
